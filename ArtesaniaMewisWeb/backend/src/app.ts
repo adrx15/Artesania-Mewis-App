6c4943cbae3a92e63017a63094b814ca52567b84
unknown <sistemas@VRE_LAB20615.inacap.cl>
2026-07-15 12:07:44 -0400

    conexión render-vercel

--- ArtesaniaMewisWeb/backend/src/app.ts
+++ ArtesaniaMewisWeb/backend/src/app.ts
@@ -20,7 +20,7 @@ app.use(helmet());
 
 // Configuración explícita de CORS para entorno de desarrollo local
 app.use(cors({
-    origin: 'http://localhost:5173', // El puerto exacto donde corre el frontend en React con Vite
+    origin: 'artesania-mewis-app-wkte.vercel.app', // El puerto exacto donde corre el frontend en React con Vite
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
 }));
